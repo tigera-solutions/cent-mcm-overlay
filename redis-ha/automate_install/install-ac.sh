32e04c70670fa8cf49e406d6f31ad9d578f71707
Kartik Bharath <kartik@tigera.io>
2024-03-11 13:26:52 -0400

    Update that AC script is unused and why

--- redis-ha/automate_install/install-ac.sh
+++ redis-ha/automate_install/install-ac.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# NOT USED IN REPO AT THIS TIME
+# Putting Redis operator pod in hostNetwork: true mode crashes due to other ports exposed at 8080 and 443 that clash with the host ports. 
+# The AC isn't necessary here to demonstrate the Redis federated services scenario and cluster-mesh. To be explored later.
+# Reference: https://docs.tigera.io/calico/latest/getting-started/kubernetes/managed-public-cloud/eks#:~:text=%3F-,NOTE,-Calico%20networking%20cannot
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source $SCRIPT_DIR/redis-functions.sh
 
@@ -9,4 +14,4 @@ if ! [ -x "$(command -v kubectl)" ]; then
   exit 1
 fi
 
-install_redis_ac
+# install_redis_ac
