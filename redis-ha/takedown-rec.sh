2d6dd332f2fc13a39fe68a67ff1aafb79338a0be
Kartik Bharath <kartik@tigera.io>
2024-03-12 16:24:07 -0400

    Var changes to script

--- redis-ha/takedown-rec.sh
+++ redis-ha/takedown-rec.sh
@@ -1,6 +1,6 @@
 # User vars that can be changed
 REDIS_NAMESPACE=redis
-TARGET_K8S_CONTEXTS=("kartik+tigera-solutions@tigera.io@cc-eks-mcm1.ca-central-1.eksctl.io")
+TARGET_K8S_CONTEXTS=("kartik+tigera-solutions@tigera.io@cc-eks-mcm2.us-east-1.eksctl.io")
 
 # Don't touch this (unless you know what you are doing)
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
@@ -10,7 +10,7 @@ do
     echo "Changing context to K8s cluster ${TARGET_K8S_CONTEXTS[i]}"
     kubectl config use-context ${TARGET_K8S_CONTEXTS[i]}
     echo
-    TARGET_REC=$(kubectl get rec -n redis -ojsonpath='{.items..metadata.name}')
+    TARGET_REC=$(kubectl get rec -n $REDIS_NAMESPACE -ojsonpath='{.items..metadata.name}')
     echo "Putting Redis Enterprise Cluster named $TARGET_REC in namespace $REDIS_NAMESPACE into recovery mode"
     kubectl patch rec $TARGET_REC -n $REDIS_NAMESPACE --type merge --patch '{"spec":{"clusterRecovery":true}}'
     echo "Sleeping for 3 seconds before performing checks"
